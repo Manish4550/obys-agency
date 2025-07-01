e8708a14aedebae009392699dde50b530795dbe7
Manish kumar <Amity4933@gmai.com>
2025-07-01 21:55:59 +0530

    Made loader successfully

---
+++ script.js
@@ -0,0 +1,46 @@
+var tl = gsap.timeline();
+
+tl.from(".line h1", {
+  y: 150,
+  stagger: 0.25,
+  duraction: 0.6,
+  delay: 0.5,
+});
+
+tl.from("#line1-part1", {
+  opacity: 0,
+  onStart: function () {
+    var h5timer = document.querySelector("#line1-part1 h5");
+    var grow = 0;
+    setInterval(function () {
+      if (grow < 100) {
+        h5timer.innerHTML = grow++;
+      } else {
+        h5timer.innerHTML = grow;
+      }
+    }, 35);
+  },
+});
+
+tl.to(".line h2", {
+    AnimationName:"anime",
+    opacity:1,
+})
+
+tl.to("#loader", {
+  opacity: 0,
+  duraction: 0.2,
+  delay: 4,
+});
+
+tl.from("#page1",{
+    delay:0.2,
+    y:1600,
+    opacity:0,
+    duraction:0.5,
+    ease:power4
+})
+
+tl.to("#loader",{
+    display:"none",
+})
