61280e07fcbc0e5e85cd27dbe7a7e062ee86b28c
Manish kumar <Amity4933@gmai.com>
2025-07-02 12:11:01 +0530

    page4

--- script.js
+++ script.js
@@ -46,12 +46,12 @@ tl.to("#loader",{
     display:"none",
 })
 
-tl.from(".nav",{
+tl.from("#nav",{
     opacity:0,
 })
 
-tl.from("#hero1 h1,#hero2 h1, #hero3 h2, #hero4 h1",{
-    y:120,
+tl.from("#hero1 h1,#hero2 h1,#hero3 h2,#hero4 h1",{
+    y:140,
     stagger:0.2,
 })
 }
